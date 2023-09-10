9e5234c30824cd36c8526baf451c5f90157bbca4
TheMisfit68 <janverrept@me.com>
2023-09-10 20:48:24 +0200

    Updated manifest to v5.9

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "TizenDriver",
-    platforms: [.macOS(.v11)],
+    platforms: [.macOS(.v13)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -14,8 +14,8 @@ let package = Package(
     ],
     dependencies: [
 		// Dependencies declare other packages that this package depends on.
-		.package(name: "Awake", url: "https://github.com/TheMisfit68/Awake.git", .branch("master")),
-		.package(name: "JVCocoa", url: "https://github.com/TheMisfit68/JVCocoa.git", .branch("master")),
+        .package(url: "https://github.com/TheMisfit68/Awake.git", branch:"master"),
+        .package(url: "https://github.com/TheMisfit68/JVCocoa.git", branch:"master"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
