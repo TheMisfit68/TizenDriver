f454cf0dd3625badc2191e502733c649f3ab27fe
TheMisfit68 <janverrept@me.com>
2024-03-15 14:13:29 +0100

    Adjusted manifest to MacOS 14 platform

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "TizenDriver",
 	defaultLocalization: "en",
-    platforms: [.macOS(.v13)],
+    platforms: [.macOS(.v14)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
