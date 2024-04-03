cc4e8cb1d38147b84ec21f2ac998b820b90464f5
TheMisfit68 <janverrept@me.com>
2024-04-03 22:46:56 +0200

    Made TizenTypes a subtype of the module instead of the main driver class

--- Sources/TizenDriver/TizenTypes.swift
+++ Sources/TizenDriver/TizenTypes.swift
@@ -1,35 +1,29 @@
 //
 //  TizenTypes.swift
-//  
+//
 //
 //  Created by Jan Verrept on 26/03/2022.
 //
 
 import Foundation
 
-public extension TizenDriver{
+public enum PowerState:Comparable{
 	
-	 enum PowerState:Comparable{
-		
-		case poweredOff
-		case poweringDown
-		case poweringUp
-		case poweredOn
-		
-	}
+	case poweredOff
+	case poweringDown
+	case poweringUp
+	case poweredOn
+	
 }
 
-internal extension TizenDriver{
+enum ConnectionState:Comparable{
 	
-	enum ConnectionState:Comparable{
-		
-		case disconnected
-		case disconnecting
-		case connecting
-		case connected
-		case paired
-		
-	}
-	
+	case disconnected
+	case disconnecting
+	case connecting
+	case connected
+	case paired
 	
 }
+
+
